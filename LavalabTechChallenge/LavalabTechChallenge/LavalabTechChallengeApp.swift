2aac4874bfa7688e4c4487f126267b8c91e63231
amrita-arun <amrita.arun2013@gmail.com>
2024-09-12 14:58:51 -0700

    package dependencies imported, folder structure created

---
+++ LavalabTechChallenge/LavalabTechChallenge/LavalabTechChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LavalabTechChallengeApp.swift
+//  LavalabTechChallenge
+//
+//  Created by Administrator on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LavalabTechChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
